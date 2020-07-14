30bd8dfbf8efd4eb653647c54d4a1c368b24011a
YutaroM <yutaroapp@gmail.com>
2020-07-14 21:55:33 +0900

    基本的なファイルを追加

---
+++ CameMode/CameMode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameMode
+//
+//  Created by YutaroMatsunaga on 2020/07/14.
+//  Copyright © 2020 YutaroMatsunaga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
